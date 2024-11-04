17136199905b744cd54fbaa303b2c611483c5a60
Jack O'Brien <jadyobri@ucsc.edu>
2024-11-04 14:33:42 -0800

    DialogTest

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 // todo
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("myButton") as HTMLButtonElement;
+
+  if (button) {
+    button.addEventListener("click", () => {
+      alert("you clicked the button!");
+      console.log("you clicked the button!");
+    });
+  }
+});
