8936073bbf20600f9f2d8df00943acec805c582e
Jack O'Brien <jadyobri@ucsc.edu>
2024-11-05 15:36:39 -0800

    Test 1

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("myButton") as HTMLButtonElement;
 
+  //button click alert
   if (button) {
     button.addEventListener("click", () => {
       alert("you clicked the button!");
